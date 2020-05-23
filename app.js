e247201438d55fbb8139357cca3812845c808fab
Alex <alex.garcia.arias@gmail.com>
2020-05-23 16:06:45 -0500

    backend, BD, middleware

---
+++ app.js
@@ -0,0 +1,36 @@
+// Requires
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+// inicializar variables
+const app = express();
+
+// Body Parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+// Importar rutas
+const appRoutes = require('./routes/app');
+const usuarioRoutes = require('./routes/usuario');
+const loginRoutes = require('./routes/login');
+
+// conexion a la base de datos
+mongoose.connection.openUri('mongodb+srv://xxx:xxx@xxx.xxx.xxx.xxx/hospitalDB?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, (err, res) => {
+    if (err) throw err;
+    console.log('Base de Datos: \x1b[32m%s\x1b[0m', 'online');
+});
+
+
+// Rutas
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
+
+
+// Escuchar peticiones
+app.listen(3000, () => {
+    console.log('Express Server puerto 3000: \x1b[32m%s\x1b[0m', 'online');
+});
